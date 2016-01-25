6bb2c092da8b6098486093f82aab81828a0d21b2
joseph <joseph@Zipcoder06s-MacBook-Pro.local>
2016-01-25 17:25:02 -0500

    Add Initialization Lab 2 part 2

--- src/main/java/dugan/joseph/initializationLab2/InitializationLab2.java
+++ src/main/java/dugan/joseph/initializationLab2/InitializationLab2.java
@@ -58,11 +58,68 @@ class ThingContainer {
 
     }
 
-    public void printThings() {
-        for (ColorfulThing thing: colorfulThings) {
-            System.out.println(thing.toString());
+    public ColorfulThing pop() {
+        if (numOfObjs == 0) {
+            System.out.println("ColorfulThing array is empty");
+            return null;
+        }
+        else {
+            int idx = numOfObjs - 1;
+            ColorfulThing popped = colorfulThings[idx];
+            colorfulThings[idx] = null;
+            numOfObjs--;
+            return popped;
         }
     }
+
+    public ColorfulThing remove(ColorfulThing.Color color) {
+        for (int i = 0; i < colorfulThings.length; i++) {
+            if (colorfulThings[i] != null &&
+                    color.equals(colorfulThings[i].getColor())) {
+                ColorfulThing removed = colorfulThings[i];
+                colorfulThings[i] = null;
+                numOfObjs--;
+                if (i < colorfulThings.length - 1)
+                    shift(i);
+                return removed;
+            }
+        }
+        return null;
+    }
+
+    public ColorfulThing remove(ColorfulThing colorfulThing) {
+        for (int i = 0; i < colorfulThings.length; i++) {
+            ColorfulThing currentThing = colorfulThings[i];
+            if (currentThing != null &&
+                    currentThing.equals(colorfulThing)) {
+                ColorfulThing removed = currentThing;
+                colorfulThings[i] = null;
+                numOfObjs--;
+                if (i < colorfulThings.length - 1)
+                    shift(i);
+                return removed;
+            }
+        }
+        return null;
+    }
+
+    public void printThings() {
+        for (ColorfulThing thing: colorfulThings) {
+            if (thing == null)
+                System.out.println("null");
+            else
+                System.out.println(thing.toString());
+        }
+    }
+
+    private void shift(int idx) {
+        for (int i = idx; i < colorfulThings.length - 1; i++) {
+            ColorfulThing next = colorfulThings[i + 1];
+            colorfulThings[i] =  next;
+            colorfulThings[i + 1] = null;
+        }
+    }
+
 }
 
 public class InitializationLab2 {
@@ -83,14 +140,37 @@ public class InitializationLab2 {
         collection[1] = thingContainer2;
         collection[2] = thingContainer3;
 
+        ThingContainer[] resultContainer = new ThingContainer[NUM_CONTAINERS];
+        int idx = 0;
+
         for (ThingContainer thingContainer: collection) {
-            int size = random.nextInt(CONTAINER_SIZE_LIMIT);
+            int size = random.nextInt(CONTAINER_SIZE_LIMIT) + 1;
             thingContainer = new ThingContainer(size);
             for (int i = 0; i <= size; i++) {
                 thingContainer.add(new ColorfulThing(ColorfulThing.Color.getRandom()));
             }
             thingContainer.printThings();
+            resultContainer[idx] = thingContainer;
+            idx++;
         }
+
+        final ColorfulThing.Color colorToRemove = ColorfulThing.Color.BLUE;
+        ThingContainer demonstrationCollection = resultContainer[0];
+        ColorfulThing[] thingsArray = demonstrationCollection.getColorfulThings();
+        ColorfulThing thingToRemove = thingsArray[0];
+
+        System.out.println("Thing container before removes: ");
+        demonstrationCollection.printThings();
+
+        ColorfulThing removedThing1 = demonstrationCollection.remove(colorToRemove);
+        ColorfulThing removedThing2 = demonstrationCollection.remove(thingToRemove);
+
+        System.out.println("ColorfulThings removed: ");
+        System.out.println(removedThing1.toString());
+        System.out.println(removedThing2.toString());
+
+        System.out.println("Thing container after removes: ");
+        demonstrationCollection.printThings();
 
 
     }
--- src/test/java/dugan/joseph/initializationLab2/ThingContainerSpec.java
+++ src/test/java/dugan/joseph/initializationLab2/ThingContainerSpec.java
@@ -14,14 +14,14 @@ public class ThingContainerSpec {
     private final static int SIZE = 3;
 
     // Some ColorfulThing s
-    private final static ColorfulThing colorfulThing1 = new ColorfulThing(ColorfulThing.Color.BLACK);
-    private final static ColorfulThing colorfulThing2 = new ColorfulThing(ColorfulThing.Color.BLUE);
-    private final static ColorfulThing colorfulThing3 = new ColorfulThing(ColorfulThing.Color.RED);
-    private final static ColorfulThing colorfulThing4 = new ColorfulThing(ColorfulThing.Color.GREEN);
+    private static ColorfulThing colorfulThing1 = new ColorfulThing(ColorfulThing.Color.BLACK);
+    private static ColorfulThing colorfulThing2 = new ColorfulThing(ColorfulThing.Color.BLUE);
+    private static ColorfulThing colorfulThing3 = new ColorfulThing(ColorfulThing.Color.RED);
+    private static ColorfulThing colorfulThing4 = new ColorfulThing(ColorfulThing.Color.GREEN);
 
     // some ColorfulThing s array
-    private final static ColorfulThing[] loadColorfulThings = {colorfulThing1, colorfulThing2, colorfulThing3};
-    private final static ColorfulThing[] overloadColorfulThings = {colorfulThing1, colorfulThing2, colorfulThing3, colorfulThing4};
+    private static ColorfulThing[] loadColorfulThings = {colorfulThing1, colorfulThing2, colorfulThing3};
+    private static ColorfulThing[] overloadColorfulThings = {colorfulThing1, colorfulThing2, colorfulThing3, colorfulThing4};
 
     // ThingContainer s
     private ThingContainer fullThingContainer;
@@ -62,6 +62,83 @@ public class ThingContainerSpec {
         ColorfulThing[] expectedFull = loadColorfulThings;
         ColorfulThing[] actualFull = thingContainer.getColorfulThings();
         assertArrayEquals(expectedFull, actualFull);
+
+    }
+
+    private static ColorfulThing[] colorfulThingsPostPop = {colorfulThing1, colorfulThing2, null};
+    private static ColorfulThing poppedThing = colorfulThing3;
+
+    @Test
+    public void popItemOffEndOfArray() {
+        // load array
+        for (ColorfulThing thing : loadColorfulThings) {
+            fullThingContainer.add(thing);
+        }
+
+        // test popped
+        // 1. are arrays indiscernible
+        ColorfulThing poppedOff = fullThingContainer.pop();
+        ColorfulThing[] expectedArray =  colorfulThingsPostPop;
+        ColorfulThing[] actualArray = fullThingContainer.getColorfulThings();
+        assertArrayEquals(expectedArray, actualArray);
+        // 2. was the expected item popped
+        ColorfulThing expectedThing = poppedThing;
+        ColorfulThing actualThing = poppedOff;
+        assertEquals(expectedThing, actualThing);
+    }
+
+    private static ColorfulThing[] colorfulThingPostRemoveByColor = {colorfulThing1, colorfulThing3, null};
+    private static ColorfulThing expectedColorfulThingWhenThingQualifies = colorfulThing2;
+    private static ColorfulThing.Color searchByColor = colorfulThing2.getColor();
+    private static ColorfulThing expectedColorfulThingWhenNoElementQualifies = null;
+    private static ColorfulThing.Color unusedColor = ColorfulThing.Color.YELLOW;
+
+    @Test
+    public void removeItemByColor() {
+        // load array
+        for (ColorfulThing thing : loadColorfulThings) {
+            fullThingContainer.add(thing);
+        }
+
+        // test with element in array
+        ColorfulThing actualColorfulThing = fullThingContainer.remove(searchByColor);
+        assertEquals(expectedColorfulThingWhenThingQualifies, actualColorfulThing);
+
+        // test for absent element in array.
+        ColorfulThing actualColorfulThingWhenNoElementQualifies = fullThingContainer.remove(unusedColor);
+        assertEquals(expectedColorfulThingWhenNoElementQualifies, actualColorfulThingWhenNoElementQualifies);
+
+        // test arrays for indiscernibility
+        ColorfulThing[] actualArray = fullThingContainer.getColorfulThings();
+        assertArrayEquals(colorfulThingPostRemoveByColor, actualArray);
+
+
+    }
+
+    private static ColorfulThing[] colorfulThingsPostRemoveByObject = {colorfulThing2, colorfulThing3, null};
+    private static ColorfulThing colorfulThingToMatch = colorfulThing1;
+    private static ColorfulThing colorfulThingToMiss = colorfulThing4;
+    private static ColorfulThing valueOfColorfulThingWhenMissed = null;
+
+    @Test
+    public void removeItemByObject() {
+        // load array
+        for (ColorfulThing thing : loadColorfulThings) {
+            fullThingContainer.add(thing);
+        }
+
+        // test with matching object
+        ColorfulThing actualRemovedThing = fullThingContainer.remove(colorfulThingToMatch);
+        ColorfulThing expectedRemovedThing = colorfulThingToMatch;
+        assertEquals(expectedRemovedThing, actualRemovedThing);
+
+        // test with missing object
+        actualRemovedThing = fullThingContainer.remove(colorfulThingToMiss);
+        expectedRemovedThing = valueOfColorfulThingWhenMissed;
+        assertEquals(expectedRemovedThing, actualRemovedThing);
+
+        // test array
+        assertArrayEquals(fullThingContainer.getColorfulThings(), colorfulThingsPostRemoveByObject);
 
     }
 
